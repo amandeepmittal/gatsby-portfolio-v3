eb7b1c5300b4a0b9ed6089d15cdf0d3f66e98901
amandeepmittal <amandeepmittal@live.com>
2018-01-16 23:57:11 +0530

    add blog posts

---
+++ src/data/blog.js
@@ -0,0 +1,85 @@
+export const blogposts = [
+  {
+    title: "Gatsby.js: How to set up and use the React Static Site Generator ",
+    publication: "freeCodeCamp",
+    link:
+      "https://medium.freecodecamp.org/setting-up-and-getting-used-to-gatsby-1fc27985ae8a"
+  },
+  {
+    title:
+      "https://www.zeolearn.com/magazine/build-a-progressive-web-app-using-react",
+    publication: "ZeoLearn/blog",
+    link:
+      "https://www.zeolearn.com/magazine/build-a-progressive-web-app-using-react"
+  },
+  {
+    title:
+      "https://hackernoon.com/create-a-simple-twitter-bot-with-node-js-5b14eb006c08#.mkrc0cug7",
+    publication: "Hackernoon & Rising Stack",
+    link: "https://community.risingstack.com/node-js-twitter-bot-tutorial/"
+  },
+  {
+    title: "Create a Twitter Bot with Node.js Series Part II",
+    publication: "Rising Stack",
+    link:
+      "https://community.risingstack.com/how-to-make-a-twitter-bot-with-node-js/"
+  },
+  {
+    title: " The Node.js System",
+    publication: "Hackernoon",
+    link: "https://hackernoon.com/the-node-js-system-51090c35dddc"
+  },
+  {
+    title: "How to Setup a React App from Scratch?",
+    publication: "Codeburst.io",
+    link:
+      "https://codeburst.io/how-to-setup-a-react-app-from-scratch-860bad109837"
+  },
+  {
+    title: "How to Mock Data for Node.js Applications using Faker.js?",
+    publication: "Hackernoon",
+    link:
+      "https://hackernoon.com/how-to-mock-data-for-node-js-applications-using-faker-js-b1f4c0e78102"
+  },
+  {
+    title: "Getting Started with Ionic Framework: An Overview",
+    publication: "Hackernoon",
+    link:
+      "https://hackernoon.com/getting-started-with-ionic-framework-an-overview-6725b687779b#.yvj6rnijh"
+  },
+  {
+    title: "Tips for Creating Node.js REST APIs",
+    publication: "Codeburst.io",
+    link:
+      "https://codeburst.io/tips-for-creating-node-js-rest-apis-dfa0b2adb39c"
+  },
+  {
+    title: "How to use ESLint in Node.js Applications?",
+    publication: "Hackernoon",
+    link:
+      "https://hackernoon.com/how-to-use-eslint-in-node-js-applications-cc4b2298ce55"
+  },
+  {
+    title: "Ionic 2/3 CLI reference",
+    publication: "Hackernoon",
+    link: "https://hackernoon.com/ionic-2-3-cli-reference-13f9fae3f964"
+  },
+  {
+    title: "Getting Started with Sequelize for Nodejs Applications",
+    publication: "Hackernoon",
+    link:
+      "https://hackernoon.com/getting-started-with-sequelize-for-nodejs-applications-2854c58ffb8c"
+  },
+  {
+    title: "Using Google Fonts in an Ionic Application",
+    publication: "Hackernoon",
+    link:
+      "https://hackernoon.com/using-google-fonts-in-an-ionic-application-c3419c342f23"
+  },
+  {
+    title: "How to use Font Awesome in an Ionic Application",
+    publication: "Hackernoon",
+    link:
+      "https://hackernoon.com/how-to-use-font-awesome-in-an-ionic-application-31638ed3f77d"
+  }
+];
