affe9ae320372de905bb079771c27ba7474a3bc9
amandeepmittal <amandeepmittal@live.com>
2018-01-16 16:57:06 +0530

    fix: social links in center

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -5,30 +5,28 @@ import styles from "./socialLinksStyles.module.css";
 
 const SocialLinks = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <a href="mailto:amanmittal.work@gmail.com">
-            <FaEnvelope />
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/amandeepmittal">
-            <FaGithub />
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/amanhimself">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href="https://medium.com/@amanhimself">
-            <FaMedium />
-          </a>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      <li>
+        <a href="mailto:amanmittal.work@gmail.com">
+          <FaEnvelope />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/amandeepmittal">
+          <FaGithub />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/amanhimself">
+          <FaTwitter />
+        </a>
+      </li>
+      <li>
+        <a href="https://medium.com/@amanhimself">
+          <FaMedium />
+        </a>
+      </li>
+    </ul>
   );
 };
 
